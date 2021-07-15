eb253e57da754c2f61e0e2d64a750e13cd96193c
Riki420 <girottiriccardo1998@gmail.com>
2021-07-15 14:41:09 +0200

    create funzioni + controllo num doppio + stile pag

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+//--------------------------------------------------------------------------------------//
+//                                                                                      //
+//                                  //* JS_CAMPOMINATO                                  //
+//                                                                                      //
+//--------------------------------------------------------------------------------------//
+
+
 /* 
 Il computer deve generare 16 numeri casuali tra 1 e 100, queste saranno le nostre bombe.
 I numeri delle bombe non possono essere duplicati (i 16 numeri devono essere tutti diversi)
@@ -16,59 +23,133 @@ Quando la partita termina, comunichiamo all'utente il suo punteggio.
 //* 6 Verificare se il numero inserito non è presente tra i doppioni e le bombe, lo aggiungo nell'array dei numeri scelti
 
 
+//****************************************************************************************
+
+
+
+
+//--------------------------------------------------------------------------------------//
+//                                //* INITIAL VARIABLES                                 //
+//--------------------------------------------------------------------------------------//
+
 
 //creo gli array dove andrò poi ad inserire i valori del COM e del PLAYER
+var totalBombs = 16;
+var totalNumbers = 100;
 var listBombs = [];
 var listUser = [];
 var listDupli = [];
 var userNum;
-var possibilita = 5;  //! DA CAMBIARE PER AVERE UN DETERMINATO NUMERO DI TENTATIVI  84
+var possibilita = 5;  //! DA CAMBIARE PER AVERE UN DETERMINATO NUMERO DI TENTATIVI  84 (totalNumbers - totalBombs)
 var find = false;
 
-//genero i 16 numeri casuali per il COM con un ciclo while
-var i = 0;
-while(i < (16 + 1)){
-    var comNum = Math.floor(Math.random() * (100 - 16) + 1);
-    if(!listBombs.includes(comNum)){
-      listBombs.push(comNum);
 
+
+
+//--------------------------------------------------------------------------------------//
+//                                   //* NUMERI BOMBA                                   //
+//--------------------------------------------------------------------------------------//
+
+
+//genero i 16 numeri casuali per il COM con un ciclo while
+while(listBombs.length < totalBombs){
+    var comNum = getRandomNumber(1,totalNumbers);
+
+    //verifico che i numeri generati dal COM vengano inseriti solo se non doppioni
+    if(!listBombs.includes(comNum)){  //se comNum non è in listBomb lo includo
+      listBombs.push(comNum);
     }
-    i++;
 }
+
 //stampo in console l'array delle bombe
 console.log('Array BOMBS: ' + listBombs);
 
 
-//chiedo il numero all'utente per 100 - 16 volte
-var l = 0;
-while (l < possibilita && find == false) {
 
-    //prompt per l'utente
-  userNum = parseInt(prompt('Inserisci un numero da 1 a 100'));
-  console.log('Numero inserito: ' + userNum);
-  if(userNum)
-  listUser.push(userNum);
-  
-    //se il numero inserito è presente nelle bombe perdi la partita, altrimenti continua fino alla vittoria
-  for (var i = 0; i < listBombs.length; i++) {
-    if (userNum == listBombs[i]) {
-      find = true;
+
+
+
+//--------------------------------------------------------------------------------------//
+//                        //* NUMERI UTENTE  -  INIZIO DEL GIOCO                        //
+//--------------------------------------------------------------------------------------//
+
+var userLost = false;
+
+//chiedo il numero all'utente per 100 - 16 volte
+
+while (!userLost && listUser.length < possibilita) {     
+  var userNum = getUserNumber(1,totalNumbers);
+
+  if(isInArray(userNum, listBombs)){       //se userNum è in listBombs perdi la partita
+    userLost = true;
+  } else{
+    if(isInArray(userNum, listUser)){      //se un userNum è già stato scelto, crea alert
+      alert('numero già scelto');
+    } else{
+      listUser.push(userNum);             //se invece non lo è push in listUser
     }
   }
-  l++;
 }
+
+
+
+//--------------------------------------------------------------------------------------//
+//                            //* ALERT E RISULTATO PARTITA                             //
+//--------------------------------------------------------------------------------------//
+
 
 //stampo in console e in pagina il risultato della partita
-if (find == true) {
-    console.log("Sei finito su una bomba, sei caduto soldato");
-    var message = "Sei finito su una bomba, sei caduto soldato";
-    document.getElementById('display').innerHTML = message;
-  } else if(find == false){
-    console.log("Missione compiuta, l'addestramento è finito soldato");
-    var message = "Missione compiuta, l'addestramento è finito soldato";
-    document.getElementById('display').innerHTML = message;
+
+
+if(userLost){
+  alert('hai perso');
+  document.getElementById('display').innerText = 'Hai perso la partita soldato';    //se userLost è true perdi la partita
+
+} else{
+  alert('hai vinto');
+  document.getElementById('display').innerText = 'Hai vinto la partita soldato';    //se userLost è false vinci la partita
 }
 
-// ? Nel caso in cui ci fossero numeri uguali, richiedi il numero
-  //creo una funzione per non far ripetere all'utente gli stessi numeri
- 
+//variabili elementi
+bombsElement = document.getElementById('bombs').innerHTML = listBombs;
+userElement = document.getElementById('user').innerHTML = listUser;
+
+
+
+
+//--------------------------------------------------------------------------------------//
+//                                                                                      //
+//                             //* DICHIARAZIONE FUNZIONI                               //
+//                                                                                      //
+//--------------------------------------------------------------------------------------//
+
+
+
+ //funzione numeri random COM
+function getRandomNumber(min,max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//funzione per replicare un .includes()
+function isInArray(needle, arr){
+  var found = false;
+  var i = 0;
+  while(!found && i < arr.length){
+    if(needle === arr[i]){
+      found = true;
+    }
+    i++;
+  }
+  return found;
+}
+
+
+
+//funzione per chiedere i numeri all'utente
+function getUserNumber(min,max){
+  var number;
+  do{
+    number = parseInt(prompt('Inserisci un numero da ' +  min + ' a ' + totalNumbers));
+  } while(!number || isNaN(number) || number < min || number > max);
+  return parseInt(number);   
+}
